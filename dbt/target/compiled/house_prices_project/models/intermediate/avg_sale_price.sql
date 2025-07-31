22d1baa1c9fc59294a076403e0c4339bd34cf9f1
Vasu Chetty <vasuchettyphd@gmail.com>
2025-07-31 02:16:57 -0600

    removing log and target files

--- dbt/target/compiled/house_prices_project/models/intermediate/avg_sale_price.sql
+++
@@ -1,9 +0,0 @@
--- Average sale price by neighborhood and overall quality
-select
-  neighborhood,
-  overall_qual,
-  avg(sale_price) as avg_price,
-  count(*) as num_sales
-from "dbt_db"."public"."house_prices"
-group by neighborhood, overall_qual
-order by avg_price desc
